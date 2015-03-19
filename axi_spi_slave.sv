40614517a68e1b4664b2f04abf86d29209f1711c
Igor Loi <igor.loi@unibo.it>
2015-03-19 20:17:36 +0100

    added UMC 65 ROM, fixed several rtl files for synthesis

--- axi_spi_slave.sv
+++ axi_spi_slave.sv
@@ -79,45 +79,45 @@ module axi_spi_slave
     output logic                        axi_master_b_ready
 );
 
+    logic        en_quad;
+    logic  [7:0] rx_counter;
+    logic        rx_counter_upd;
+    logic [31:0] rx_data;
+    logic        rx_data_valid;
 
-        logic        en_quad;
-        logic  [7:0] rx_counter;
-        logic        rx_counter_upd;
-        logic [31:0] rx_data;
-        logic        rx_data_valid;
-        
-        logic  [7:0] tx_counter;
-        logic        tx_counter_upd;
-        logic [31:0] tx_data;
-        logic        tx_data_valid;
+    logic  [7:0] tx_counter;
+    logic        tx_counter_upd;
+    logic [31:0] tx_data;
+    logic        tx_data_valid;
 
-        logic        ctrl_rd_wr;
-        
-        logic [31:0] ctrl_addr;
-        logic        ctrl_addr_valid;
+    logic        ctrl_rd_wr;
 
-        logic [31:0] ctrl_data_rx;
-        logic        ctrl_data_rx_valid;
-        logic        ctrl_data_rx_ready;
-        logic [31:0] ctrl_data_tx;
-        logic        ctrl_data_tx_valid;
-        logic        ctrl_data_tx_ready;
+    logic [31:0] ctrl_addr;
+    logic        ctrl_addr_valid;
 
-        logic [31:0] fifo_data_rx;
-        logic        fifo_data_rx_valid;
-        logic        fifo_data_rx_ready;
-        logic [31:0] fifo_data_tx;
-        logic        fifo_data_tx_valid;
-        logic        fifo_data_tx_ready;
-        
-        logic [31:0] addr_sync;
-        logic        addr_valid_sync;
-        logic        cs_sync;
-        
-        logic        tx_done;
-        logic        rd_wr_sync;
-        
-        spi_slave_rx u_rxreg (
+    logic [31:0] ctrl_data_rx;
+    logic        ctrl_data_rx_valid;
+    logic        ctrl_data_rx_ready;
+    logic [31:0] ctrl_data_tx;
+    logic        ctrl_data_tx_valid;
+    logic        ctrl_data_tx_ready;
+
+    logic [31:0] fifo_data_rx;
+    logic        fifo_data_rx_valid;
+    logic        fifo_data_rx_ready;
+    logic [31:0] fifo_data_tx;
+    logic        fifo_data_tx_valid;
+    logic        fifo_data_tx_ready;
+
+    logic [31:0] addr_sync;
+    logic        addr_valid_sync;
+    logic        cs_sync;
+
+    logic        tx_done;
+    logic        rd_wr_sync;
+
+    spi_slave_rx u_rxreg 
+    (
         .sclk(spi_sclk),
         .cs(spi_cs),
         .sdi0(spi_sdi0),
@@ -129,10 +129,11 @@ module axi_spi_slave
         .counter_in_upd(rx_counter_upd),
         .data(rx_data),
         .data_ready(rx_data_valid)
-            );
+    );
 
-        spi_slave_tx u_txreg(
-                .test_mode(test_mode),
+    spi_slave_tx u_txreg
+    (
+        .test_mode(test_mode),
         .sclk(spi_sclk),
         .cs(spi_cs),
         .sdo0(spi_sdo0),
@@ -145,141 +146,156 @@ module axi_spi_slave
         .data(tx_data),
         .data_valid(tx_data_valid),
         .done(tx_done)
-            );
-        
-        spi_slave_controller #(.DUMMY_CYCLES(DUMMY_CYCLES)) u_slave_sm(
-                .sclk(spi_sclk),
-                .sys_rstn(axi_aresetn),
-                .cs(spi_cs),
-                .en_quad(en_quad),
-                .pad_mode(spi_mode),
-                .rx_counter(rx_counter),
-                .rx_counter_upd(rx_counter_upd),
-                .rx_data(rx_data),
-                .rx_data_valid(rx_data_valid),
-                .tx_counter(tx_counter),
-                .tx_counter_upd(tx_counter_upd),
-                .tx_data(tx_data),
-                .tx_data_valid(tx_data_valid),
-                .tx_done(tx_done),
-                .ctrl_rd_wr(ctrl_rd_wr),
-                .ctrl_addr(ctrl_addr),
-                .ctrl_addr_valid(ctrl_addr_valid),
-                .ctrl_data_rx(ctrl_data_rx),
-                .ctrl_data_rx_valid(ctrl_data_rx_valid),
-                .ctrl_data_rx_ready(ctrl_data_rx_ready),
-                .ctrl_data_tx(ctrl_data_tx),
-                .ctrl_data_tx_valid(ctrl_data_tx_valid),
-                .ctrl_data_tx_ready(ctrl_data_tx_ready)
-                );
-        
-        spi_slave_dc_fifo #(
-                        .DATA_WIDTH(32),
-                        .BUFFER_DEPTH(8)
-                        ) u_dcfifo_rx (
-                        .clk_a(spi_sclk),
-                        .rstn_a(axi_aresetn),
-                        .data_a(ctrl_data_rx),
-                        .valid_a(ctrl_data_rx_valid),
-                        .ready_a(ctrl_data_rx_ready),
-                        .clk_b(axi_aclk),
-                        .rstn_b(axi_aresetn),
-                        .data_b(fifo_data_rx),
-                        .valid_b(fifo_data_rx_valid),
-                        .ready_b(fifo_data_rx_ready)
-                        );
+    );
 
-        spi_slave_dc_fifo #(
-                        .DATA_WIDTH(32),
-                        .BUFFER_DEPTH(8)
-                        ) u_dcfifo_tx (
-                        .clk_a(axi_aclk),
-                        .rstn_a(axi_aresetn),
-                        .data_a(fifo_data_tx),
-                        .valid_a(fifo_data_tx_valid),
-                        .ready_a(fifo_data_tx_ready),
-                        .clk_b(spi_sclk),
-                        .rstn_b(axi_aresetn),
-                        .data_b(ctrl_data_tx),
-                        .valid_b(ctrl_data_tx_valid),
-                        .ready_b(ctrl_data_tx_ready)
-                        );
-                
-        spi_slave_axi_plug #(
-                        .AXI_ADDR_WIDTH(AXI_ADDR_WIDTH),
-                        .AXI_DATA_WIDTH(AXI_DATA_WIDTH),
-                        .AXI_USER_WIDTH(AXI_USER_WIDTH),
-                        .AXI_ID_WIDTH(AXI_ID_WIDTH)
-                        ) u_axiplug (
-                        .axi_aclk(axi_aclk), 
-                        .axi_aresetn(axi_aresetn),
-                        .axi_master_aw_valid(axi_master_aw_valid), 
-                        .axi_master_aw_addr(axi_master_aw_addr), 
-                        .axi_master_aw_prot(axi_master_aw_prot), 
-                        .axi_master_aw_region(axi_master_aw_region),
-                        .axi_master_aw_len(axi_master_aw_len), 
-                        .axi_master_aw_size(axi_master_aw_size), 
-                        .axi_master_aw_burst(axi_master_aw_burst), 
-                        .axi_master_aw_lock(axi_master_aw_lock), 
-                        .axi_master_aw_cache(axi_master_aw_cache), 
-                        .axi_master_aw_qos(axi_master_aw_qos), 
-                        .axi_master_aw_id(axi_master_aw_id), 
-                        .axi_master_aw_user(axi_master_aw_user), 
-                        .axi_master_aw_ready(axi_master_aw_ready), 
-                        .axi_master_ar_valid(axi_master_ar_valid), 
-                        .axi_master_ar_addr(axi_master_ar_addr), 
-                        .axi_master_ar_prot(axi_master_ar_prot), 
-                        .axi_master_ar_region(axi_master_ar_region),
-                        .axi_master_ar_len(axi_master_ar_len), 
-                        .axi_master_ar_size(axi_master_ar_size), 
-                        .axi_master_ar_burst(axi_master_ar_burst), 
-                        .axi_master_ar_lock(axi_master_ar_lock), 
-                        .axi_master_ar_cache(axi_master_ar_cache), 
-                        .axi_master_ar_qos(axi_master_ar_qos), 
-                        .axi_master_ar_id(axi_master_ar_id), 
-                        .axi_master_ar_user(axi_master_ar_user), 
-                        .axi_master_ar_ready(axi_master_ar_ready), 
-                        .axi_master_w_valid(axi_master_w_valid), 
-                        .axi_master_w_data(axi_master_w_data), 
-                        .axi_master_w_strb(axi_master_w_strb), 
-                        .axi_master_w_user(axi_master_w_user), 
-                        .axi_master_w_last(axi_master_w_last), 
-                        .axi_master_w_ready(axi_master_w_ready), 
-                        .axi_master_r_valid(axi_master_r_valid), 
-                        .axi_master_r_data(axi_master_r_data), 
-                        .axi_master_r_resp(axi_master_r_resp), 
-                        .axi_master_r_last(axi_master_r_last), 
-                        .axi_master_r_id(axi_master_r_id), 
-                        .axi_master_r_user(axi_master_r_user), 
-                        .axi_master_r_ready(axi_master_r_ready), 
-                        .axi_master_b_valid(axi_master_b_valid), 
-                        .axi_master_b_resp(axi_master_b_resp), 
-                        .axi_master_b_id(axi_master_b_id), 
-                        .axi_master_b_user(axi_master_b_user), 
-                        .axi_master_b_ready(axi_master_b_ready),
-                        .rxtx_addr(addr_sync),
-                        .rxtx_addr_valid(addr_valid_sync),
-                        .start_tx(rd_wr_sync & addr_valid_sync),
-                        .cs(cs_sync),
-                        .tx_data(fifo_data_tx),
-                        .tx_valid(fifo_data_tx_valid),
-                        .tx_ready(fifo_data_tx_ready),
-                        .rx_data(fifo_data_rx),
-                        .rx_valid(fifo_data_rx_valid),
-                        .rx_ready(fifo_data_rx_ready)
-                        );      
-        
-        spi_slave_syncro u_syncro(
-                        .sys_clk(axi_aclk),
-                        .rstn(axi_aresetn),
-                        .cs(spi_cs),
-                        .address(ctrl_addr),
-                        .address_valid(ctrl_addr_valid),
-                        .rd_wr(ctrl_rd_wr),
-                        .cs_sync(cs_sync),
-                        .address_sync(addr_sync),
-                        .address_valid_sync(addr_valid_sync),
-                        .rd_wr_sync(rd_wr_sync)
-                        );                       
-                       
+    spi_slave_controller
+    #(
+        .DUMMY_CYCLES(DUMMY_CYCLES)
+    )
+    u_slave_sm
+    (
+        .sclk(spi_sclk),
+        .sys_rstn(axi_aresetn),
+        .cs(spi_cs),
+        .en_quad(en_quad),
+        .pad_mode(spi_mode),
+        .rx_counter(rx_counter),
+        .rx_counter_upd(rx_counter_upd),
+        .rx_data(rx_data),
+        .rx_data_valid(rx_data_valid),
+        .tx_counter(tx_counter),
+        .tx_counter_upd(tx_counter_upd),
+        .tx_data(tx_data),
+        .tx_data_valid(tx_data_valid),
+        .tx_done(tx_done),
+        .ctrl_rd_wr(ctrl_rd_wr),
+        .ctrl_addr(ctrl_addr),
+        .ctrl_addr_valid(ctrl_addr_valid),
+        .ctrl_data_rx(ctrl_data_rx),
+        .ctrl_data_rx_valid(ctrl_data_rx_valid),
+        .ctrl_data_rx_ready(ctrl_data_rx_ready),
+        .ctrl_data_tx(ctrl_data_tx),
+        .ctrl_data_tx_valid(ctrl_data_tx_valid),
+        .ctrl_data_tx_ready(ctrl_data_tx_ready)
+    );
+
+    spi_slave_dc_fifo
+    #(
+        .DATA_WIDTH(32),
+        .BUFFER_DEPTH(8)
+    ) 
+    u_dcfifo_rx
+    (
+        .clk_a(spi_sclk),
+        .rstn_a(axi_aresetn),
+        .data_a(ctrl_data_rx),
+        .valid_a(ctrl_data_rx_valid),
+        .ready_a(ctrl_data_rx_ready),
+        .clk_b(axi_aclk),
+        .rstn_b(axi_aresetn),
+        .data_b(fifo_data_rx),
+        .valid_b(fifo_data_rx_valid),
+        .ready_b(fifo_data_rx_ready)
+    );
+
+    spi_slave_dc_fifo
+    #(
+        .DATA_WIDTH(32),
+        .BUFFER_DEPTH(8)
+    )
+    u_dcfifo_tx
+    (
+        .clk_a(axi_aclk),
+        .rstn_a(axi_aresetn),
+        .data_a(fifo_data_tx),
+        .valid_a(fifo_data_tx_valid),
+        .ready_a(fifo_data_tx_ready),
+        .clk_b(spi_sclk),
+        .rstn_b(axi_aresetn),
+        .data_b(ctrl_data_tx),
+        .valid_b(ctrl_data_tx_valid),
+        .ready_b(ctrl_data_tx_ready)
+    );
+
+    spi_slave_axi_plug 
+    #(
+        .AXI_ADDR_WIDTH(AXI_ADDR_WIDTH),
+        .AXI_DATA_WIDTH(AXI_DATA_WIDTH),
+        .AXI_USER_WIDTH(AXI_USER_WIDTH),
+        .AXI_ID_WIDTH(AXI_ID_WIDTH)
+    )
+    u_axiplug
+    (
+        .axi_aclk(axi_aclk), 
+        .axi_aresetn(axi_aresetn),
+        .axi_master_aw_valid(axi_master_aw_valid), 
+        .axi_master_aw_addr(axi_master_aw_addr), 
+        .axi_master_aw_prot(axi_master_aw_prot), 
+        .axi_master_aw_region(axi_master_aw_region),
+        .axi_master_aw_len(axi_master_aw_len), 
+        .axi_master_aw_size(axi_master_aw_size), 
+        .axi_master_aw_burst(axi_master_aw_burst), 
+        .axi_master_aw_lock(axi_master_aw_lock), 
+        .axi_master_aw_cache(axi_master_aw_cache), 
+        .axi_master_aw_qos(axi_master_aw_qos), 
+        .axi_master_aw_id(axi_master_aw_id), 
+        .axi_master_aw_user(axi_master_aw_user), 
+        .axi_master_aw_ready(axi_master_aw_ready), 
+        .axi_master_ar_valid(axi_master_ar_valid), 
+        .axi_master_ar_addr(axi_master_ar_addr), 
+        .axi_master_ar_prot(axi_master_ar_prot), 
+        .axi_master_ar_region(axi_master_ar_region),
+        .axi_master_ar_len(axi_master_ar_len), 
+        .axi_master_ar_size(axi_master_ar_size), 
+        .axi_master_ar_burst(axi_master_ar_burst), 
+        .axi_master_ar_lock(axi_master_ar_lock), 
+        .axi_master_ar_cache(axi_master_ar_cache), 
+        .axi_master_ar_qos(axi_master_ar_qos), 
+        .axi_master_ar_id(axi_master_ar_id), 
+        .axi_master_ar_user(axi_master_ar_user), 
+        .axi_master_ar_ready(axi_master_ar_ready), 
+        .axi_master_w_valid(axi_master_w_valid), 
+        .axi_master_w_data(axi_master_w_data), 
+        .axi_master_w_strb(axi_master_w_strb), 
+        .axi_master_w_user(axi_master_w_user), 
+        .axi_master_w_last(axi_master_w_last), 
+        .axi_master_w_ready(axi_master_w_ready), 
+        .axi_master_r_valid(axi_master_r_valid), 
+        .axi_master_r_data(axi_master_r_data), 
+        .axi_master_r_resp(axi_master_r_resp), 
+        .axi_master_r_last(axi_master_r_last), 
+        .axi_master_r_id(axi_master_r_id), 
+        .axi_master_r_user(axi_master_r_user), 
+        .axi_master_r_ready(axi_master_r_ready), 
+        .axi_master_b_valid(axi_master_b_valid), 
+        .axi_master_b_resp(axi_master_b_resp), 
+        .axi_master_b_id(axi_master_b_id), 
+        .axi_master_b_user(axi_master_b_user), 
+        .axi_master_b_ready(axi_master_b_ready),
+        .rxtx_addr(addr_sync),
+        .rxtx_addr_valid(addr_valid_sync),
+        .start_tx(rd_wr_sync & addr_valid_sync),
+        .cs(cs_sync),
+        .tx_data(fifo_data_tx),
+        .tx_valid(fifo_data_tx_valid),
+        .tx_ready(fifo_data_tx_ready),
+        .rx_data(fifo_data_rx),
+        .rx_valid(fifo_data_rx_valid),
+        .rx_ready(fifo_data_rx_ready)
+    );
+
+    spi_slave_syncro u_syncro
+    (
+        .sys_clk(axi_aclk),
+        .rstn(axi_aresetn),
+        .cs(spi_cs),
+        .address(ctrl_addr),
+        .address_valid(ctrl_addr_valid),
+        .rd_wr(ctrl_rd_wr),
+        .cs_sync(cs_sync),
+        .address_sync(addr_sync),
+        .address_valid_sync(addr_valid_sync),
+        .rd_wr_sync(rd_wr_sync)
+    );
+
 endmodule
--- spi_slave_rx.sv
+++ spi_slave_rx.sv
@@ -1,89 +1,87 @@
-module spi_slave_rx(
-        input  logic        sclk,
-        input  logic        cs,
-        input  logic        sdi0,
-        input  logic        sdi1,
-        input  logic        sdi2,
-        input  logic        sdi3,
-        input  logic        en_quad_in,
-        input  logic  [7:0] counter_in,
-        input  logic        counter_in_upd,
-        output logic [31:0] data,
-        output logic        data_ready
-        );
-	
-	reg [31:0] data_int;
-	reg [31:0] data_int_next;
-	reg  [7:0] counter;
-	reg  [7:0] counter_trgt;
-	reg  [7:0] counter_next;
-	reg  [7:0] counter_trgt_next;
-	
-	logic      running;
-	logic      running_next;
+module spi_slave_rx
+(
+    input  logic        sclk,
+    input  logic        cs,
+    input  logic        sdi0,
+    input  logic        sdi1,
+    input  logic        sdi2,
+    input  logic        sdi3,
+    input  logic        en_quad_in,
+    input  logic  [7:0] counter_in,
+    input  logic        counter_in_upd,
+    output logic [31:0] data,
+    output logic        data_ready
+);
 
-	assign data = data_int_next;
-	
-	always_comb
-	begin
-		
-		if (counter_in_upd)
-			counter_trgt_next = counter_in;
-		else if ((counter_trgt == 8'h1) && !en_quad_in)
-			counter_trgt_next = 8'h7;
-		else
-			counter_trgt_next = counter_trgt;
+    reg [31:0] data_int;
+    reg [31:0] data_int_next;
+    reg  [7:0] counter;
+    reg  [7:0] counter_trgt;
+    reg  [7:0] counter_next;
+    reg  [7:0] counter_trgt_next;
 
-		if (counter_in_upd)
-			running_next = 1'b1;
-		else if (counter == counter_trgt)
-			running_next = 1'b0;
-		else
-			running_next = running;
-		
-		if (running)
-		begin
-			if (counter == counter_trgt)
-			begin
-				counter_next =  'h0;
-				data_ready   = 1'b1;
-			end
-			else
-			begin
-				counter_next = counter + 1;
-				data_ready   = 1'b0;
-			end
-			if (en_quad_in)
-				data_int_next = {data_int[27:0],sdi3,sdi2,sdi1,sdi0};
-			else
-				data_int_next = {data_int[30:0],sdi0};
-		end
-		else
-		begin
-			counter_next  = counter;
-			data_ready    = 1'b0;
-			data_int_next = data_int;
-		end
-	end
-	
-	
-	always @(posedge sclk or posedge cs)
-	begin
-		if (cs == 1'b1)
-		begin
-			counter      = 0;
-			counter_trgt = 'h1;
-			data_int     = 'h0;
-			running      = 'h1;
-		end
-		else
-		begin
-			counter      = counter_next;
-			counter_trgt = counter_trgt_next;
-			data_int     = data_int_next;
-			running      = running_next;
-		end
-	end
+    logic      running;
+    logic      running_next;
+
+    assign data = data_int_next;
+
+    always_comb
+    begin
+        if (counter_in_upd)
+            counter_trgt_next = counter_in;
+        else if ((counter_trgt == 8'h1) && !en_quad_in)
+                counter_trgt_next = 8'h7;
+             else
+                counter_trgt_next = counter_trgt;
+
+        if (counter_in_upd)
+            running_next = 1'b1;
+        else if (counter == counter_trgt)
+                running_next = 1'b0;
+             else
+                running_next = running;
+
+        if (running)
+        begin
+                if (counter == counter_trgt)
+                begin
+                    counter_next =  'h0;
+                    data_ready   = 1'b1;
+                end
+                else
+                begin
+                    counter_next = counter + 1;
+                    data_ready   = 1'b0;
+                end
+                if (en_quad_in)
+                    data_int_next = {data_int[27:0],sdi3,sdi2,sdi1,sdi0};
+                else
+                    data_int_next = {data_int[30:0],sdi0};
+        end
+        else
+        begin
+            counter_next  = counter;
+            data_ready    = 1'b0;
+            data_int_next = data_int;
+        end
+    end
+
+
+    always @(posedge sclk or posedge cs)
+    begin
+        if (cs == 1'b1)
+        begin
+            counter      = 0;
+            counter_trgt = 'h1;
+            data_int     = 'h0;
+            running      = 'h1;
+        end
+        else
+        begin
+            counter      = counter_next;
+            counter_trgt = counter_trgt_next;
+            data_int     = data_int_next;
+            running      = running_next;
+        end
+    end
 endmodule
-
-
