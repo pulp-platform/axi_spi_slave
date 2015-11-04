a58ddee4c8da07049e8444f1d82c9382c29232e5
Antonio Pullini <pullinia@fenga2.ee.ethz.ch>
2015-11-04 13:41:22 +0100

    Fix to how addr_ready is synch between domains

--- spi_slave_syncro.sv
+++ spi_slave_syncro.sv
@@ -16,27 +16,27 @@ module spi_slave_syncro
 		);
 	
 	logic [1:0] cs_reg;
-	logic [1:0] valid_reg;
+	logic [2:0] valid_reg;
 	logic [1:0] rdwr_reg;
 	
 	assign cs_sync = cs_reg[1];
-	assign address_valid_sync = valid_reg[1];
+	assign address_valid_sync = ~valid_reg[2] & valid_reg[1]; //detect rising edge of addr valid
 	assign address_sync = address;
 	assign rd_wr_sync = rdwr_reg[1];
 	
 	always @(posedge sys_clk or negedge rstn)
 	begin
-		if(rstn == 0)
+		if(rstn == 1'b0)
 		begin
-			cs_reg     =  2'b11;
-			valid_reg  =  2'b00;
-			rdwr_reg   =  2'b00;
+			cs_reg     <=  2'b11;
+			valid_reg  <=  3'b000;
+			rdwr_reg   <=  2'b00;
 		end
 		else
 		begin
-			cs_reg     = {cs_reg[0],cs};
-			valid_reg  = {valid_reg[0],address_valid};
-			rdwr_reg   = {rdwr_reg[0],rd_wr};
+			cs_reg     <= {cs_reg[0],cs};
+			valid_reg  <= {valid_reg[1:0],address_valid};
+			rdwr_reg   <= {rdwr_reg[0],rd_wr};
 		end
 	end
 	
